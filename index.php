859b0aad811d730377f48abe90169b1603f95966
Christophe HENRY <christophe.henry@sbgodin.fr>
2021-03-16 20:54:10 +0100

    Deep refactoring: parsing, translating, classes
    * Removes "^" to disable text decoration line-wise.
    * Split HTML generation in two: parsing and translating.
    * Create class to handle gemtext parsing.
    * Create class to translate to HTML.
    * Create class to generate back gemtext (for future test cases).
    * Uses generators to parse then translate.
    * Fix: 404 doesn't occur for an empty file.
    * Page 404 fully generated by HtmGem itself.
    * CSS is no longer incorporated in the HTML page.
    * Handle CSS inclusion by addCss() calls.

--- index.php
+++ index.php
@@ -1,91 +1,61 @@
 <?php
 
 require_once "lib-htmgem.php";
-use function htmgem\translateGemToHtml;
 
-# to false only if textDecoration=0 in the URL
-$textDecoration = "0" != @$_REQUEST['textDecoration'];
-
-/* The url argument is always absolute compared to the document root
- * The leading slash is removed. so url=/foo/bar and url=foo/bar ar the same.
- */
+# The url argument is always absolute compared to the document root.
 $url = @$_REQUEST["url"];
 
-######################################## Installation page
+/* Installation page
+ *
+ * Accessing directly /htmgem will make display the self-hosted documentation
+ * contained in "index.gmi". If it's removed, diplay an empty page with a
+ * comment
+ */
 if (empty($url)) {
     if (!file_exists("index.gmi")) {
         http_response_code(403);
         die("<!-- index.gmi missing -->");
     }
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<title>Installation de HtmGem</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style>
-<?php include("css/htmgem.css"); ?>
-</style>
-</head>
-<body>
-<?php
-    echo translateGemToHtml(@file_get_contents("index.gmi"));
-    echo "</body>\n</html>\n";
-    die();
+    $t = new \htmgem\GemTextTranslate_html(@file_get_contents("index.gmi"));
+    echo $t->getFullHtml();
+    exit();
 }
-######################################## /Installation page
+
+
+# to false only if textDecoration=0 in the URL
+$textDecoration = "0" != @$_REQUEST['textDecoration'];
 
 # Removes the headling and trailling slashes, to be sure there's not any.
 $filePath = rtrim($_SERVER['DOCUMENT_ROOT'], "/")."/".ltrim($url, "/");
 
-$fileContents = @file_get_contents($filePath);
-
-
-######################################## 404 page
-if (empty($fileContents)) {
+/* 404 page
+ */
+if (!file_exists($filePath)) {
     error_log("HtmGem: 404 $url $filePath");
-    http_response_code(404); ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style>
-<?php include("css/htmgem.css"); ?>
-</style>
-</head>
-<body>
-<?php
-    $text404 = <<<EOF
+    http_response_code(404);
+    $page404 = <<<EOF
 # ⚠ Page non trouvée
 
 ​**$url**
 
 => $url Recharger 🔄
-
 => /
 EOF;
-echo translateGemToHtml($text404);
-echo "</body>\n</html>";
-die();
+    $t = new \htmgem\GemTextTranslate_html($page404);
+    echo $t->getFullHtml();
+    exit();
 }
-######################################## /404 page
 
+
+$fileContents = @file_get_contents($filePath);
 # Removes the Byte Order Mark
 $fileContents = preg_replace("/\xEF\xBB\xBF/", "", $fileContents);
 
-# Gets the page title: the first occurrence with # at the line start
-mb_ereg("#\s*([^\n]+)\n", $fileContents, $matches);
-$page_title = @$matches[1];
 
-###################################### CSS Management
-/**
-* if &style=source displays the source directly and stops.
-* if there's a filename.css besides filename.gmi, use the css and stops.
-* if &style=<NOTHING> then embbed the default style, and stops.
-* if &style=<word not beginngin by slash> then use htmgem/word.css
-* if &style=/… then use the … as as stylesheet.
-**/
+/* CSS and special style management
+ */
 
+$style = @$_REQUEST['style'];
 if ("source" == $style) {
     $basename = basename($filePath);
     header("Cache-Control: public");
@@ -93,9 +63,12 @@ if ("source" == $style) {
     header("Content-Type: text/plain");
     header("Content-Transfer-Encoding: binary");
     header('Content-Length: ' . filesize($filePath));
-    readfile($filePath);
+    echo $fileContents;
     exit();
 } elseif ("pre" == $style) {
+    # Gets the page title: the first occurrence with # at the line start
+    mb_ereg("#\s*([^\n]+)\n", $fileContents, $matches);
+    $page_title = @$matches[1];
     $fileContents = htmlspecialchars($fileContents, ENT_HTML5|ENT_NOQUOTES, "UTF-8", false);
     echo <<<EOL
 <!DOCTYPE html>
@@ -104,52 +77,32 @@ if ("source" == $style) {
 <title>$page_title</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
-<pre>$fileContents</pre>
+<pre>
+$fileContents</pre>
 </body>
 </html>
 EOL;
+    exit();
 } else {
-    $parts = pathinfo($filePath);
-    $localCss = $parts["filename"].".css";
-    $localCssFilePath = $parts["dirname"]."/".$localCss;
-    if (file_exists($localCssFilePath)) {
-        # Warning, using htmhem.php?url=… will make $localCss not found
-        # as the path is relative to htmgem.php and not / !
-        $cssContent = "<link type='text/css' rel='StyleSheet' href='$localCss'>";
-    } else {
-        if (empty($style)) {
-            $cssContent =
-                 "<style>\n"
-                .@file_get_contents("css/htmgem.css")
-                ."</style>\n";
-        } else {
-            if ("none" == $style) {
-                $cssContent = "";
-            } else {
-                if ("/" == $style[0])
-                    $href = $style;
-                else
-                    $href = "/htmgem/css/$style.css";
-                $cssContent = "<link type='text/css' rel='StyleSheet' href='$href'>";
-            }
+    $t = new \htmgem\GemTextTranslate_html($fileContents);
+    if ("none" == $style) {
+        $t->addCss("");
+    } elseif ("/" == @$style[0]) {
+        $t->addCss($style);
+    } elseif (empty($style)) {
+        $parts = pathinfo($filePath);
+        $localCss = $parts["filename"].".css";
+        $localCssFilePath = $parts["dirname"]."/".$localCss;
+        if (file_exists($localCssFilePath)) {
+            # Warning, using htmhem.php?url=… will make $localCss not found
+            # as the path is relative to htmgem.php and not / !
+            $t->addCss($localCss);
         }
+    } else { #TODO: regex check for $style
+        $t->addCss("/htmgem/css/$style.css");
     }
-    echo <<<EOL
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<title>$page_title</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-$cssContent
-</head>
-<body>
-EOL;
-
-    echo "\n".translateGemToHtml($fileContents);
-    echo "</body>\n</html>\n";
 }
 
-
-ob_end_flush();
+echo $t->getFullHtml();
 
 ?>
