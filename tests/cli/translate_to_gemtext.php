2261dac656364f6d2ad5753377e08a1d4754b93c
Christophe HENRY <christophe.henry@sbgodin.fr>
2021-03-25 19:08:57 +0100

    FIX: translate_to_gemtext.php

--- tests/cli/translate_to_gemtext.php
+++ tests/cli/translate_to_gemtext.php
@@ -5,6 +5,5 @@ $fileName = $argv[1];
 require_once dirname(__FILE__)."/../../lib-htmgem.php";
 
 $text = file_get_contents($fileName);
-$parsedGemtext = \htmgem\gemtextParser($text);
-$gt_gemtext = new \htmgem\GemtextTranslate_gemtext($parsedGemtext);
+$gt_gemtext = new \htmgem\GemtextTranslate_gemtext($text);
 echo strval($gt_gemtext);
